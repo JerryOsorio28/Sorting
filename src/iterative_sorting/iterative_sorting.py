63ed663e3e7765ae982fab2829291902277361f7
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-01-21 16:24:48 -0500

    finished mvp part 1

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,34 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
+def selection_sort(arr):
+    # we iterate over the array
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
-        # TO-DO: swap
-
-
-
-
+        # set the smallest value to be the current index
+        smallest = i
+        # second loop to compare values with i as we iterate
+        for j in range(i, len(arr)):
+            # if whats on index j is less than what it is currently consider as the smallest...
+            if arr[j] < arr[smallest]:
+                # we set the smallest to be the current index
+                smallest = j
+        # once we have the true smallest value, we grab the value of index i first...
+        temp = arr[i]
+        # we swap by setting what's currently the value of i to be the new smallest..
+        arr[i] = arr[smallest]
+        # then we swaps what's the smallest to be what is temp (which is what it was the value of i)
+        arr[smallest] = temp
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    # first iterator is to keep a track of the numbers than has been sorted (bubbled up)
+    for i in range(0, len(arr) - 1):
+        # second iterator is to make a comparison between the current index and the index after that..
+        for j in range(0, len(arr) - 1 - i):
+            # if it is greater..
+            if arr[j] > arr[j + 1]:
+                # we swap places...
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
 
 
